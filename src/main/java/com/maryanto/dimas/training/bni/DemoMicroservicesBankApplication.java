1416f446ee3d9d6b5af64bf30c64c4a2e621c606
dimasm93 <software.dimas_m@icloud.com>
2023-12-15 20:15:07 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/training/bni/DemoMicroservicesBankApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.training.bni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroservicesBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroservicesBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maryanto/dimas/training/bni/DemoMicroservicesBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.training.bni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroservicesBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
